f9b5789ea492d9ae6518caee67a0a6327bd4a444
Steven Van Gucht <steven@onehappyhound.be>
2025-11-17 16:21:48 +0100

    Fix: Voeg CSP headers toe voor Next.js + NextAuth + Leaflet - allow unsafe-eval voor Next.js runtime

--- next.config.js
+++ next.config.js
@@ -51,6 +51,18 @@ const nextConfig = {
             key: 'Referrer-Policy',
             value: 'origin-when-cross-origin',
           },
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src * data: blob:",
+              "connect-src *",
+              "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com",
+              "frame-src *",
+            ].join('; '),
+          },
         ],
       },
     ]
